92e0c97abb5cc47e86780961abd4a13a740a92dc
sangitasarkar74 <sangitasarkar@gmail.com>
2023-01-11 13:51:46 +0530

    more changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
-const App = () => {
-//code here
+const App = (props) => {
+  //code here
+  const [name, setName] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleChange = (event) => {
+    // return `Hello my name is ${prop}____ and I study at Newton School`
+    setName(event.target.value);
+    // p.target.value;
+    console.log(event.target.value)
+  }
+
+  const handleClickBtn = (e) => {
+    setData(name);
+  }
+
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
-    </div>
+      <input id='input' type="text" name="firstName" onChange={handleChange} value={name}></input>
+      <button id='button' onClick={handleClickBtn} value={name}>Click</button>
+      <p id='text'> Hello my name is {data}____ and I study at Newton School</p>
+    </div >
   )
 }
 
